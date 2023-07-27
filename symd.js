329f019d54ed1c05ec2b4cadcc9eb0f682b26950
shresh <shreshthmehta1@gmail.com>
2023-07-27 11:06:43 +1000

    lint

--- symd.js
+++ symd.js
@@ -33,31 +33,31 @@ console.log(syma([1, 2, 3], [5, 2, 1, 4], [7,8]));
 
 // ***********************************************************************
 //Solution 2
-const diff = (arr1, arr2) => [
-    ...arr1.filter(e => !arr2.includes(e)),
-    ...arr2.filter(e => !arr1.includes(e))
-  ];
-const sym = (...args) => [...new Set(args.reduce(diff))];
+// const diff = (arr1, arr2) => [
+//     ...arr1.filter(e => !arr2.includes(e)),
+//     ...arr2.filter(e => !arr1.includes(e))
+//   ];
+// const sym = (...args) => [...new Set(args.reduce(diff))];
 // console.log(sym([1, 2, 3], [5, 2, 1, 4]));
 //************************************************************************ 
 //set
-const numbers = [2, 3, 4, 4, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 5, 32, 3, 4, 5];
+// const numbers = [2, 3, 4, 4, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 5, 32, 3, 4, 5];
 // console.log([...new Set(numbers)]);
 
 //filter
 
-const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
-const result = words.filter(word => word.length > 6);
+// const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+// const result = words.filter(word => word.length > 6);
 // console.log(result);
 
 //reduce
-const array1 = [1, 2, 3, 4];
+// const array1 = [1, 2, 3, 4];
 
-const initialValue = 0;
-const sumWithInitial = array1.reduce(
-  (accumulator, currentValue) => accumulator + currentValue,
-  initialValue
-);
+// const initialValue = 0;
+// const sumWithInitial = array1.reduce(
+//   (accumulator, currentValue) => accumulator + currentValue,
+//   initialValue
+// );
 
 // console.log(sumWithInitial);
 // console.log([1, 2, 4, 5].reduce((a, b) => a + b)); // 7
